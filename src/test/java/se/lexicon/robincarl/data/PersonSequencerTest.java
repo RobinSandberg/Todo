9c36ba0656f413d749bf764505b48a9aece5a389
RobinSandberg <38344081+RobinSandberg@users.noreply.github.com>
2020-02-26 13:32:15 +0100

    data package added to test.

---
+++ src/test/java/se/lexicon/robincarl/data/PersonSequencerTest.java
@@ -0,0 +1,15 @@
+package se.lexicon.robincarl.data;
+
+import org.junit.Test;
+
+public class PersonSequencerTest {
+
+    @Test
+    public void increase_Id_Successfully(){
+        int id = 0;
+        int result;
+
+        PersonSequencer id1 = new PersonSequencer();
+        
+    }
+}
